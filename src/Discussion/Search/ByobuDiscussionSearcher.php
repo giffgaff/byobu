e1e2271127b25fd86cc1b7fe3ae7604061c37075
tariqwalji <tariqwalji@yahoo.co.uk>
2021-04-29 11:45:49 +0100

    COM-718 Fixed user filter for private discussion

--- src/Discussion/Search/ByobuDiscussionSearcher.php
+++ src/Discussion/Search/ByobuDiscussionSearcher.php
@@ -8,6 +8,7 @@ use Flarum\Discussion\Search\DiscussionSearcher;
 use Flarum\Search\GambitManager;
 use Flarum\Search\SearchCriteria;
 use Flarum\Search\SearchResults;
+use Flarum\User\UserRepository;
 use Illuminate\Contracts\Events\Dispatcher;
 use FoF\Byobu\Database\RecipientsConstraint;
 
@@ -16,9 +17,31 @@ class ByobuDiscussionSearcher extends DiscussionSearcher
 {
     use RecipientsConstraint;
 
+    protected $users;
+
+    /**
+     * @param GambitManager $gambits
+     * @param DiscussionRepository $discussions
+     * @param Dispatcher $events
+     */
+    public function __construct(GambitManager $gambits, DiscussionRepository $discussions, Dispatcher $events, UserRepository $users)
+    {
+        DiscussionSearcher::__construct($gambits, $discussions, $events);
+        $this->users = $users;
+    }
+
     private function getPrivateDiscussions(SearchCriteria $criteria, $limit = null, $offset = 0)
     {
         $actor = $criteria->actor;
+
+        $filterByUser = null;
+        foreach(explode(' ', $criteria->query) as $bit) {
+            preg_match('/^byobu:(.+)$/i', $bit, $queryPattern);
+            if ($queryPattern) {
+                list($pattern, $username) = $queryPattern;
+                $filterByUser = $this->users->findOrFailByUsername($username);
+            }
+        }
 
         $query = $this->discussions
             ->query()
@@ -27,6 +50,18 @@ class ByobuDiscussionSearcher extends DiscussionSearcher
             ->join('recipients', 'recipients.discussion_id', '=', 'discussions.id')
             ->whereNull('recipients.removed_at')
             ->where('recipients.user_id', $actor->id);
+
+
+
+        if ($filterByUser) {
+            $filterByUserQuery = $this->discussions->query()
+                ->select('discussions.id')
+                ->join('recipients', 'recipients.discussion_id', '=', 'discussions.id')
+                ->whereNull('recipients.removed_at')
+                ->where('recipients.user_id', $filterByUser->id);
+
+            $query->whereIn('discussions.id', $filterByUserQuery);
+        }
 
         if ($this->flagsInstalled()
             && $actor->hasPermission('user.viewPrivateDiscussionsWhenFlagged')
